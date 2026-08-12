0228bbbff849f3e4327ec1d782bf8da63970cdf5
Wagner Costa <seluzdoamanha@gmail.com>
2026-08-12 15:59:08 -0300

    feat: add dark mode support to form fields and implement phone number masking in pedido.js

--- atendimento/pedido.js
+++ atendimento/pedido.js
@@ -7,6 +7,13 @@
     document.addEventListener('DOMContentLoaded', () => {
         const form = document.getElementById('formAtendimento');
         form.addEventListener('submit', salvarPedido);
+
+        // Máscara de telefone
+        const telefoneInput = document.getElementById('inTelefone');
+        telefoneInput.addEventListener('input', function (e) {
+            let x = e.target.value.replace(/\D/g, '').match(/(\d{0,2})(\d{0,5})(\d{0,4})/);
+            e.target.value = !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '');
+        });
     });
 
     async function salvarPedido(e) {
